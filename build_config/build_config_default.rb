2743225e0337bd66cde120549bc054298c8c87d9
Charlotte Koch <dressupgeekout@gmail.com>
2021-01-04 22:47:15 -0800

    Explicitly link libmruby against libm
    Seems to be necessary on NetBSD, and it's a smarter thing to do in general

--- build_config/build_config_default.rb
+++ build_config/build_config_default.rb
@@ -9,7 +9,7 @@ MRuby::Build.new do |conf|
   conf.linker do |ld|
     ld.link_options = %Q(%{flags} -o "%{outfile}" %{objs} %{libs})
     ld.library_paths << "#{MRUBY_ROOT}/../oniguruma/build"
-    ld.libraries = ["onig",]
+    ld.libraries = ["m", "onig",]
   end
 
   conf.gembox 'default'
